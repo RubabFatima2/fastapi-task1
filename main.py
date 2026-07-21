4be0c8eed4689eff71b0403cf0588cbd2349f547
RubabFatima2 <fatimarubab6612@gmail>
2026-07-22 00:12:08 +0500

    Stage 3: create with validation

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class newtask(BaseModel):
+    title : str
+  
 app = FastAPI()
 
 tasks = [{"id":0, "title":"glossary", "done":True},
@@ -14,9 +19,26 @@ async def wealth():
 async def health():
     return { "status": "ok" }
 
+@app.get("/tasks")
+async def get_task():
+    return {"tasks:":tasks}
+
+
 @app.get("/tasks/{id}")
 async def all_list(id:int):
     for task in tasks:
         if task["id"] == id:
             return task
     return {"error": f"Task 99 has no id"}
+
+@app.post("/tasks")
+async def add_tasks(task: newtask):
+    next_id = max(task["id"] for task in tasks) + 1
+    done = False
+    new_task = { "id": next_id,"title": task.title,"done":done }
+    if task.title.strip() == "":
+        return {"error":404}
+    else:
+        tasks.append(new_task)
+        return {"task" : new_task}
+    
