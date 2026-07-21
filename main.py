9421ed13b1d0ba11bd1b5a1495787ff8a898300a
RubabFatima2 <fatimarubab6612@gmail>
2026-07-21 16:21:30 +0500

    Stage 2: read endpoints with 404

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 from fastapi import FastAPI
 app = FastAPI()
 
-
+tasks = [{"id":0, "title":"glossary", "done":True},
+        {"id":1, "title":"shopping", "done":True},
+        {"id":3, "title":"work", "done":False} ]
 
 @app.get("/")
 async def wealth():
@@ -12,3 +14,9 @@ async def wealth():
 async def health():
     return { "status": "ok" }
 
+@app.get("/tasks/{id}")
+async def all_list(id:int):
+    for task in tasks:
+        if task["id"] == id:
+            return task
+    return {"error": f"Task 99 has no id"}
